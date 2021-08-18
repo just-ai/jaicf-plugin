e45ceae3c5c106503faada8d2eeaade441ee0cac
Protasov Vitalii <veptechno@gmail.com>
2021-08-18 17:28:47 +0300

    contexts: replace deprecated isInContext method by new one

--- src/main/kotlin/com/justai/jaicf/plugin/contexts/StateContext.kt
+++ src/main/kotlin/com/justai/jaicf/plugin/contexts/StateContext.kt
@@ -1,11 +1,11 @@
 package com.justai.jaicf.plugin.contexts
 
+import com.intellij.codeInsight.template.TemplateActionContext
 import com.intellij.codeInsight.template.TemplateContextType
-import com.intellij.psi.PsiFile
 import com.justai.jaicf.plugin.getBoundedCallExpressionOrNull
 import com.justai.jaicf.plugin.services.isStateDeclaration
 
 class StateContext : TemplateContextType("STATE", "State") {
-    override fun isInContext(file: PsiFile, offset: Int) =
-        file.findElementAt(offset)?.getBoundedCallExpressionOrNull()?.isStateDeclaration == true
+    override fun isInContext(templateActionContext: TemplateActionContext) = templateActionContext.file
+        .findElementAt(templateActionContext.startOffset)?.getBoundedCallExpressionOrNull()?.isStateDeclaration == true
 }
