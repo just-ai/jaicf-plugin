314eb66e6f8af21efcaeb93c5f20494aa47699fd
Protasov Vitalii <veptechno@gmail.com>
2021-11-09 11:52:49 +0300

    contexts: improve state context detection

--- src/main/kotlin/com/justai/jaicf/plugin/contexts/StateContext.kt
+++ src/main/kotlin/com/justai/jaicf/plugin/contexts/StateContext.kt
@@ -3,9 +3,14 @@ package com.justai.jaicf.plugin.contexts
 import com.intellij.codeInsight.template.TemplateActionContext
 import com.intellij.codeInsight.template.TemplateContextType
 import com.justai.jaicf.plugin.getBoundedCallExpressionOrNull
+import com.justai.jaicf.plugin.getBoundedLambdaArgumentOrNull
 import com.justai.jaicf.plugin.scenarios.psi.builders.isStateDeclaration
+import org.jetbrains.kotlin.psi.KtCallExpression
 
 class StateContext : TemplateContextType("STATE", "State") {
-    override fun isInContext(templateActionContext: TemplateActionContext) = templateActionContext.file
-        .findElementAt(templateActionContext.startOffset)?.getBoundedCallExpressionOrNull()?.isStateDeclaration == true
+    override fun isInContext(templateActionContext: TemplateActionContext): Boolean {
+        val probeElement = templateActionContext.file.findElementAt(templateActionContext.startOffset)
+        val lambdaArgument = probeElement?.getBoundedLambdaArgumentOrNull(KtCallExpression::class.java) ?: return false
+        return lambdaArgument.getBoundedCallExpressionOrNull()?.isStateDeclaration == true
+    }
 }
